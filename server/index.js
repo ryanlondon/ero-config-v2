d8144399a836ad5b6ac824770430f165d6b64575
ryanlondon <ryel590@gmail.com>
2018-06-05 18:32:40 -0700

    added basic api routes for db testing

--- server/index.js
+++ server/index.js
@@ -1,16 +1,24 @@
+require('dotenv/config');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const os = require('os');
 
-const interfaces = os.networkInterfaces();
-const IP = interfaces.en0[3].address;
+const projectController = require('./controllers/projectController');
+const roomController = require('./controllers/roomController');
+
 const PORT = 3000;
 
 const app = express();
+
+// --- Use express middleware ------- //
+app.use(bodyParser.json());
 
 // --- Serve static assets ---------- //
 app.use(express.static(path.join(__dirname, '../src/')));
 app.use(express.static(path.join(__dirname, '../')));
 
-app.listen(PORT, () => console.log(`Server listening at http://${IP}:${PORT}`));
+// --- API routes ------------------- //
+app.get('/api/projects/all', projectController.getAllProjects);
+app.get('api/rooms', roomController.getRooms);
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`));
