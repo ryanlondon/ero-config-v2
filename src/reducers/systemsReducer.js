092548ceb4436e3b6839805f3a09537514912480
ryanlondon <ryel590@gmail.com>
2018-06-12 21:47:25 -0700

    created systemsReducer

---
+++ src/reducers/systemsReducer.js
@@ -0,0 +1,14 @@
+import * as types from '../actions/actionTypes';
+
+const initialState = [];
+
+const systemsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case types.UPDATE_SYSTEMS:
+      return [...state, ...action.systems];
+    default:
+      return state;
+  }
+}
+
+export default systemsReducer;
