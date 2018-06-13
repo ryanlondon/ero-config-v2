0c4540e58eaed0a9d2d875373f24f6f6d8c2d3fe
ryanlondon <ryel590@gmail.com>
2018-06-12 21:45:50 -0700

    added systems prop

--- src/components/SystemSelect.jsx
+++ src/components/SystemSelect.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-const SystemSelect = () => (
-  <div>
-    <select id="project-select">
-      <option>New Project</option>
-      <option>Project 1</option>
-    </select>
-    <button type="button" id="project-load">Load</button>
-  </div>
-);
+const SystemSelect = ({ systems }) => {
+  const options = systems.map(system => (
+    <option id={system._id} key={system._id}>{system.name}</option>
+  ));
+  options.unshift(<option id="0">New System</option>);
+
+  return (
+    <div>
+      <select id="project-select">
+        {options}
+      </select>
+      <button type="button" id="project-load">Load</button>
+    </div>
+  );
+};
 
 export default SystemSelect;
