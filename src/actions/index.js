cb454d9dafbdb25669a4e40dd5d35b187ac842e2
ryanlondon <ryel590@gmail.com>
2018-06-12 21:42:50 -0700

    added systems actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,3 +4,19 @@ export const navigate = page => ({
   type: types.NAVIGATE,
   page,
 });
+
+export const updateSystems = systems => ({
+  type: types.UPDATE_SYSTEMS,
+  systems,
+});
+
+export const fetchSystems = () => {
+  // console.log('FETCH SYSTEMS');
+  return (dispatch) => {
+    // console.log('FETCH SYSTEMS INNER');
+    return fetch('/api/systems')
+      .then(res => res.json())
+      .then(json => dispatch(updateSystems(json)))
+      .catch(err => console.error('<< ERROR >> Error fetching systems:', err));
+  };
+};
