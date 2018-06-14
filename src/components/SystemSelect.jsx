b8eb5ee284a2bdb3bb7e90dcc5df900693171098
ryanlondon <ryel590@gmail.com>
2018-06-13 19:07:52 -0700

    added key to "new system" option

--- src/components/SystemSelect.jsx
+++ src/components/SystemSelect.jsx
@@ -4,7 +4,7 @@ const SystemSelect = ({ systems }) => {
   const options = systems.map(system => (
     <option id={system._id} key={system._id}>{system.name}</option>
   ));
-  options.unshift(<option id="0">New System</option>);
+  options.unshift(<option id="0" key="0">New System</option>);
 
   return (
     <div>
